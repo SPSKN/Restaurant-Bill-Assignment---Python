ec1fb99dbac9b264a0db47591c23ca003b426a3e
SPSKN <s.patterson.skn@gmail.com>
2021-07-12 00:24:40 -0600

    Working-No rounding
    Working without rounding, small error for money

--- bill_splitter.py
+++ bill_splitter.py
@@ -1,27 +1,18 @@
 from decimal import *
 
 def main():
-    totalBill = input('How much is the bill? ')
-    b = Decimal(totalBill)
-    print(b)
-    tax = input('How much is tax (%)? ')
-    print(tax.count(tax))
-    taxPercent = Decimal(percentConvert(tax))
-    print(b*taxPercent)
+    total_bill = Decimal(input('How much is the bill? '))
+    total_tax = int(input('How much is the tax (%)? '))
+    total_tip = int(input('How much is the tip (%)? '))
+    total_person = int(input('How many people are paying? '))
 
-
-
-def percentConvert(t):
-    st = str(t)
-    if st.count(st)==1:
-        t2 = f'1.0{st}'
-        return t2
-    elif st.count(st)==2:
-        t3 = f'1.{st}'
-        return t3
-    else:
-        tE = st
-        return tE
+    total_after_tax = ((Decimal(total_tax/100) +1) * total_bill)
+    total_after_tip = ((Decimal(total_tip/100) +1) * total_after_tax)
+    payment = total_after_tip / total_person
+    print(f'Bill before tax: {total_bill}')
+    print(f'Bill after tax: {total_after_tax}')
+    print(f'Bill after tip: {total_after_tip}')
+    print(f'Bill per person: {payment}')
 
 
 main()
